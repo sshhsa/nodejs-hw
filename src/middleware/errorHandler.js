24f2b1e51e23da334016544626ff5baa8dc9af03
Alex Marynkevych <alexandr.mr253@gmail.com>
2026-08-26 09:58:39 +0300

    fix: use named isHttpError import

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,7 +1,7 @@
-import createHttpError from 'http-errors';
+import { isHttpError } from 'http-errors';
 
 export const errorHandler = (err, _req, res, _next) => {
-  if (createHttpError.isHttpError(err)) {
+  if (isHttpError(err)) {
     return res.status(err.status).json({
       message: err.message,
     });
