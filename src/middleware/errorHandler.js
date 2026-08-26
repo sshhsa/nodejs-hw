038742867632a81c0b2855e16c0eb0b46ed96457
Alex Marynkevych <alexandr.mr253@gmail.com>
2026-08-26 09:47:46 +0300

    fix: address homework review feedback

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,7 +1,13 @@
-export const errorHandler = (err, _req, res, _next) => {
-  const status = err.status || 500;
+import createHttpError from 'http-errors';
 
-  res.status(status).json({
+export const errorHandler = (err, _req, res, _next) => {
+  if (createHttpError.isHttpError(err)) {
+    return res.status(err.status).json({
+      message: err.message,
+    });
+  }
+
+  res.status(500).json({
     message: err.message,
   });
 };
