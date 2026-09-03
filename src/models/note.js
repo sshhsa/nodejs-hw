56f005661c44ed580eb9b36e01250144d03ce2d3
Alex Marynkevych <alexandr.mr253@gmail.com>
2026-09-03 14:12:51 +0300

    Add tag index to note schema

--- src/models/note.js
+++ src/models/note.js
@@ -1,4 +1,5 @@
 import { Schema, model } from 'mongoose';
+
 import { TAGS } from '../constants/tags.js';
 
 const noteSchema = new Schema(
@@ -8,21 +9,24 @@ const noteSchema = new Schema(
       required: true,
       trim: true,
     },
+
     content: {
       type: String,
       default: '',
       trim: true,
     },
+
     tag: {
       type: String,
       enum: TAGS,
       default: 'Todo',
-      index: true,
     },
   },
   {
     timestamps: true,
   },
 );
+
+noteSchema.index({ tag: 1 });
 
 export const Note = model('Note', noteSchema);
