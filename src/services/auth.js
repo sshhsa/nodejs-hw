49a2fbe33897860009ede766095d127157faf073
Alex Marynkevych <alexandr.mr253@gmail.com>
2026-09-06 16:14:15 +0300

    Fixed two moments got hw goit

--- src/services/auth.js
+++ src/services/auth.js
@@ -18,7 +18,8 @@ export const createSession = async (userId) => {
 export const setSessionCookies = (res, session) => {
   res.cookie('accessToken', session.accessToken, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
+    secure: true,
+    // secure: process.env.NODE_ENV === 'production',
     sameSite: 'none',
     maxAge: FIFTEEN_MINUTES,
   });
